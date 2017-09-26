bde9cc2c31ec3a480c2d282d7fbce6bea2e021e2
Khyber Sen <kkysen@gmail.com>
2017-09-26 18:52:22 -0400

    added css and gzip compression

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ __date__ = '2017-09-22'
 
 from flask import Flask
 from flask import render_template
+from flask_compress import Compress
 
+from minify import minify
 from occupations import Occupations
 
 occupations = Occupations.in_united_states()
@@ -23,10 +25,12 @@ app = Flask(__name__)
 @app.route('/occupations')
 def render_occupations():
     # type: () -> str
-    return render_template('occupations.jinja2', occupations=occupations.occupations,
+    return render_template('occupations.jinja2', occupations=occupations,
                            random_occupation=occupations.random_occupation())
 
 
 if __name__ == '__main__':
     app.debug = True
+    # minify(app)
+    Compress(app)
     app.run()
